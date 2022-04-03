cc5a90f8b77991cfea119855a0f04815b3197f05
Mark Hahn <mark@hahnca.com>
2022-04-03 13:06:34 -0700

    switched to pug

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Mellow Vue 3 + Vite" />
+<template pug>
+  img(alt="Vue logo" src="./assets/logo.png")
+    HelloWorld(msg="Mellow Vue 3 + Vite")
 </template>
 
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,3 @@
-<script setup>
-import { ref } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const count = ref(0)
-</script>
-
 <template>
   <h1>{{ msg }}</h1>
 
@@ -32,6 +22,16 @@ const count = ref(0)
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+</script>
 
 <style scoped>
 a {
