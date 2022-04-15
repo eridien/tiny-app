fbcb2530d6d715fa95e712f7f33101ccaea54dd9
mark hahn <mark@hahnca.com>
2022-04-15 14:28:16 -0700

    added controls component

---
+++ src/components/controls.vue
@@ -0,0 +1,23 @@
+<template lang='pug'>
+#ctrls(style="border:2px solid black")
+  #accelerator
+    | hello
+  #wheel
+</template>
+
+<script setup>
+  import {onMounted} from 'vue'
+
+  // commands to bot
+  const setAccel = (accel) => this.$emit('setAccel', accel);
+  const setYaw   = (yaw)   => this.$emit('setYaw',   yaw);
+  const stop     = ()      => this.$emit('stop');
+  const pwrOff   = ()      => this.$emit('pwrOff');
+
+  onMounted(async () => { 
+
+  });
+</script>
+
+<style>
+</style>
