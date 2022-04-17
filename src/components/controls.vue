f130622786418dddbe42512a4f2feb8f713f738c
Mark Hahn <mark@hahnca.com>
2022-04-17 09:51:49 -0700

    rotate steering-wheel

--- src/components/controls.vue
+++ src/components/controls.vue
@@ -3,11 +3,12 @@
               display:flex;")
   #accelerator(style="border:1px solid blue; width:25%;          \
                       min-height: calc(100vh - 100px);")
-  #wheel(style="border:1px solid red; width:75%; display:flex; \
-                      min-height: calc(100vh - 100px); \
+  #wheel(style="border:1px solid red; width:75%; display:flex;   \
+                      min-height: calc(100vh - 100px);            \
                       justify-content:center; align-items:center;")
-    img(:src="`images/steering-wheel.png`" 
-        :style="{width:'60vmin',height:'60vmin'}")
+    img(id="wheel" src="images/steering-wheel.png"
+          :style="{ transform:`rotate(${angle}deg)` }")
+
 </template>
 
 <script setup>
@@ -18,11 +19,15 @@
   const setYaw   = (yaw)   => this.$emit('setYaw',   yaw);
   const stop     = ()      => this.$emit('stop');
   const pwrOff   = ()      => this.$emit('pwrOff');
-
+  const angle    = 90;
   onMounted(async () => { 
 
   });
 </script>
 
 <style>
+#wheel{
+  width: 60vmin; 
+  height:60vmin;
+}
 </style>
