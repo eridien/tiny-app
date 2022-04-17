54966c938ad4f04a9596247c63aefb4db7a461ef
Mark Hahn <mark@hahnca.com>
2022-04-17 11:27:37 -0700

    added hamburger.vue

---
+++ src/components/hamburger.vue
@@ -0,0 +1,23 @@
+<template lang='pug'>
+div
+  img(id="hamburger" :src="`images/hamburger.png`" 
+     :style="{width:'25px', height:'25px', \
+               margin:'10px 20px 40px 0'}"
+     @click="hamburgerClick")
+</template>
+
+<script setup>
+import {onMounted, watch, ref} from 'vue'
+
+const hamburgerClick = () => {
+  console.log('hamburgerClick');
+}
+
+onMounted(async () => { 
+  console.log(`\n---- hamburger mounted ----\n`);
+});
+
+</script>
+
+<style>
+</style>
