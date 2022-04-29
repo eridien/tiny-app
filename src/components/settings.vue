423afd74516481d1a1e4a1fda0d0d4e364d72b7f
Mark Hahn <mark@hahnca.com>
2022-04-29 15:46:32 -0700

    added settings vue

---
+++ src/components/settings.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+#settings(style="border-radius:12px; font-size:28px; \
+                 background-color:#c8c8c8;           \
+                 margin:20px; padding:20px;")
+                   
+  input(type="range")
+
+</template>
+
+<script setup>
+  // const emit = defineEmits(['steering']);
+
+</script>
+
+<style>
+</style>
