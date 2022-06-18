62c0b5d8b4369104caeeaea17bf42438d6d4b395
Mark Hahn <mark@hahnca.com>
2022-06-18 13:12:36 -0700

    fixed release script

--- release.ps1
+++ release.ps1
@@ -3,7 +3,7 @@
 #  cd \Users\mark\apps\tiny
 #  C:\Users\mark\.platformio\penv\Scripts\pio run --target uploadfs
 
- cd \Users\mlap\apps\tiny-app
+ cd \Users\mark\apps\tiny-app
  vite build
- cd \Users\mlap\apps\tiny
- C:\Users\mlap\.platformio\penv\Scripts\pio run --target uploadfs
+ cd \Users\mark\apps\tiny
+ C:\Users\mark\.platformio\penv\Scripts\pio run --target uploadfs
