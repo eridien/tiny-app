fadcd800b1b758bb898eb1d65ffb65eb74fbad06
Mark Hahn <mark@hahnca.com>
2022-07-29 16:53:07 -0700

    added net mac addrs

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ import {createApp} from 'vue'
 import App         from './App.vue'
 import mitt        from 'mitt'
 
-// const BOT_HOSTNAME_DEV = "192.168.1.198" // 6-4 t-bot    red C8:C9:A3:73:35:DB
-// const BOT_HOSTNAME_DEV = "192.168.1.218" // 7-3 Cameron  green 
-// const BOT_HOSTNAME_DEV = "192.168.1.219" // 7-4 Wyatt    silver 
-// const BOT_HOSTNAME_DEV = "192.168.1.220" // 7-5 Bowie    blue 
+// const BOT_HOSTNAME_DEV = "192.168.1.198" // 6-4 t-bot   red    C8:C9:A3:73:35:DB
+// const BOT_HOSTNAME_DEV = "192.168.1.218" // 7-3 Cameron green  AC:0B:FB:E2:6D:9D
+// const BOT_HOSTNAME_DEV = "192.168.1.219" // 7-4 Wyatt   silver AC:0B:FB:E2:6E:21
+// const BOT_HOSTNAME_DEV = "192.168.1.220" // 7-5 Bowie   blue   AC:0B:FB:E0:68:0F
 
-192.168.1.199 6-5 Tb  red -- bad?
-192.168.1.198 6-4 t-bot test red C8:C9:A3:73:35:DB
-192.168.1.218 7-3 Cammeron green 
-192.168.1.219 7-4 Wyatt    silver 
-192.168.1.220 7-5 Bowie    blue 
+// 192.168.1.199 6-5 Tb  red -- bad?
+// 192.168.1.198 6-4 t-bot test red C8:C9:A3:73:35:DB
+// 192.168.1.218 7-3 Cammeron green 
+// 192.168.1.219 7-4 Wyatt    silver 
+// 192.168.1.220 7-5 Bowie    blue 
 
 
 // -- old --
